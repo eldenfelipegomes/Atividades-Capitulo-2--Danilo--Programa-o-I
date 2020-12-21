234d82d0452a4a3c4a177cc77b481d209a408b3b
Elden Felipe Gomes De Freitas <eldenfelipe2016@aluno.ueg.br>
2020-12-20 23:38:44 -0200

    first commit

---
+++ Numero 2.java
@@ -0,0 +1,11 @@
+package cap02;
+
+
+public class Exemplo2 {
+    public static void main(String args[]) {
+        int matricula = 1089, idade = 20;
+        double dolar = 2.62;
+
+        System.out.println("Matricula: " +matricula+ "\n idade: " +idade+ "\n dolar: " +dolar);
+    }
+}
